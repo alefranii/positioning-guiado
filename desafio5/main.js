8e74532f0240184dde73c3672760189d32f71e7e
alefranii <afmormar@gmail.com>
2017-05-22 15:29:46 -0400

    todos los desafios con enlace directo desde index, no funciono el 5

---
+++ desafio5/main.js
@@ -0,0 +1,16 @@
+//trayendonos los elementos que utilizaremos
+
+var contenedor = document.getElementsByClassName("contenedor")[0];
+var rosa = document.getElementById("rosa");
+var azul = document.getElementById("azul");
+var verde =getElementById("verde");
+var amarillo = document.getElementById("amarillo");
+
+contenedor.style.position = "relative";
+contenedor.style.background = "Darkgray";
+contenedor.stye.width = "600px";
+contenedor.style.padding = "50px";
+
+azul.style.position = "absolute";
+azul.style.top = "250px";
+azul.style.right = "0";
